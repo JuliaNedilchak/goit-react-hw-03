20eae5d563ef57b0f4a7e0bfbd7a8d3abf6f125c
JuliaNedilchak <julianedilchak@gmail.com>
2024-04-19 14:45:09 +0100

    validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 const App = () => {
   const initialContactList = [
@@ -10,17 +11,42 @@ const App = () => {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ];
-  const [contacts, setContacts] = useState(initialContactList);
-  const [filter, setFilter] = useState(" ");
+  const [contacts, setContacts] = useState(() => {
+    const stringified = localStorage.getItem("contacts");
+    if (!stringified) return initialContactList;
+    const parsed = JSON.parse(stringified);
+    return parsed;
+  });
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onAddContact = (formData) => {
+    const newList = {
+      ...formData,
+      id: nanoid(),
+    };
+    setContacts([...contacts, newList]);
+  };
+
+  const [filter, setFilter] = useState("");
   const onChangeContact = (event) => {
     setFilter(event.target.value);
+  };
+  const filteredList = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLocaleLowerCase())
+  );
+  const onDeleteContact = (userId) => {
+    setContacts((prevList) =>
+      prevList.filter((contact) => contact.id !== userId)
+    );
   };
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onAddContact={onAddContact} />
       <SearchBox filter={filter} onChangeContact={onChangeContact} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={filteredList} onDeleteContact={onDeleteContact} />
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,15 @@
 import Contact from "../Contact/Contact";
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <ul>
       {contacts.map((contact) => {
         return (
           <Contact
             key={contact.id}
+            id={contact.id}
             name={contact.name}
             number={contact.number}
+            onDeleteContact={onDeleteContact}
           />
         );
       })}
